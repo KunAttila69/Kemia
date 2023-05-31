f988f123da5e6808ba0854904d827342a6fb0b43
KunAttila69 <71005372+KunAttila69@users.noreply.github.com>
2023-05-31 10:21:09 +0200

    Add files via upload

---
+++ Felfedezes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Felfedezes
+    {
+        string ev;
+        string nev;
+        string vegyjel;
+        int rendszam;
+        string felfedezo;
+
+        public Felfedezes(string ev, string nev, string vegyjel, int rendszam, string feldedezo)
+        {
+            this.ev = ev;
+            this.nev = nev;
+            this.vegyjel = vegyjel;
+            this.rendszam = rendszam;
+            this.felfedezo = feldedezo;
+        }
+
+
+        public string Ev { get => ev; set => ev = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Vegyjel { get => vegyjel; set => vegyjel = value; }
+        public int Rendszam { get => rendszam; set => rendszam = value; }
+        public string Felfedezo { get => felfedezo; set => felfedezo = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Felfedezes> felfedezesek = new List<Felfedezes>();
+            StreamReader sr = new StreamReader("felfedezesek.csv");
+
+            while (!sr.EndOfStream)
+            {
+                string[] data = sr.ReadLine().Split(';');
+                if (data[1]!= "Elem")
+                {
+
+                    felfedezesek.Add(new Felfedezes(data[0], data[1], data[2], Convert.ToInt32(data[3]), data[4]));
+                }
+            }
+            Console.WriteLine($"3.feladat: Elemek száma: {felfedezesek.Count}");
+
+            int x = 0;
+            foreach (var felfedezes in felfedezesek)
+            {
+                if (felfedezes.Ev == "?kor")
+                {
+                    x += 1;
+                }
+            }
+            Console.WriteLine($"4.feladat: Felfedezések száma az ókorban: {x}");
+
+            Console.ReadKey();
+        }
+    }
+}
